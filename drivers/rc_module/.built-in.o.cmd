eb19634d5822c938cf94509f1f49540f42517b04
esevan <pes9488@gmail.com>
2015-06-10 19:31:08 +0900

    directory re-arrangement

--- drivers/rc_module/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/rc_module/built-in.o :=  /home/esevan/tizen-sdk/tools/arm-linux-gnueabi-gcc-4.5/bin/arm-linux-gnueabi-ld -EL    -r -o drivers/rc_module/built-in.o drivers/rc_module/rc_module.o 
